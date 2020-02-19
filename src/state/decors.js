63d2a41380b120b7e0b75755960e27de57d0291e
= <=>
2020-02-19 09:11:06 +0100

    Added colors of decors

---
+++ src/state/decors.js
@@ -0,0 +1,18 @@
+
+export const GOLD = {
+    name: 'gold',
+    metalnessColor: '#6f5d40',
+    color: '#d6c68f'
+};
+
+export const SILVER = {
+    name: 'silver',
+    metalnessColor: '#747474',
+    color: '#ececec'
+};
+
+export const COPPER = {
+    name: 'copper',
+    metalnessColor: '#915638',
+    color: '#fccaa3'
+}
